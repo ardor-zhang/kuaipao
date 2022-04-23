b0a722a9f902babab142ddcfcda0ee9a7163832c
ardor-zhang <arodr-zhang@outlook.com>
2022-04-23 10:15:24 +0800

    feat: basic components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+import OptimizationPersist from 'vite-plugin-optimize-persist';
+import PkgConfig from 'vite-plugin-package-config';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    PkgConfig(),
+    OptimizationPersist(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 });
